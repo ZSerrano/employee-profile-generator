3ae7d9ac63cb447c5b088fc988d2905da5836a80
Brometheus <zs.serrano1@gmail.com>
2022-08-17 21:11:01 -0500

    wrote inquirer prompts and html addition variables

--- index.js
+++ index.js
@@ -7,24 +7,149 @@ const Intern = require('./lib/intern');
 const Manager = require('./lib/manager')
 
 // Utilize inquirer to gather input information from user
-inquirer 
-    .prompt([
+const initialPrompt = () => {
+    return inquirer.prompt([
         {
-            type: '',
-            message: '',
-            name: '' 
+            type: 'checkbox',
+            message: 'What type of employee are you creating?',
+            name: 'initial',
+            choices: ['Manager', 'Engineer', 'Intern'], 
         }
-    ]) 
-// Write a .then method to return the input data into an employees.json file to store information
-
-// Classes can be used to pull their information from the employees.json file
+    ])}; 
 
 
+const managerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the employee?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is the id number for the employee?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the email address for the employee?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the office number of the manager?',
+            name: 'officeNumber',
+        }
+    ])
+}
 
+const engineerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the employee?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is the id number for the employee?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the email address for the employee?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the link to the github profile of the engineer?',
+            name: 'github',
+        },
+    ])
+}
 
+const internPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the employee?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'What is the id number for the employee?',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'What is the email address for the employee?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What school does the intern attend?',
+            name: 'school',
+        },
+    ])
+}
 
-// Create a card to hold all employee information
-// Create heading to hold employee position
-// Create text body to hold employee data
-// Append heading and text body to card
-// Append card to html in specific div
+const managerHTML = ({name, email, id, officeNumber}) =>
+`<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Employee Profiles</title>
+    <link rel="reset" href="./reset.css">
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <header>
+        My Team
+    </header>
+
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="name">Name: ${name}</h1>
+        <h1 class="position>Position: Manager</h1>
+        <h2 class="email">Email: ${email}</h2>
+        <h2 class="id">ID: ${id}</h2>
+        <h2 class="officeNumber">Office Number: ${officeNumber}</h2>
+        
+  </body>
+  </html>`;
+
+const engineerHTML = ({name, email, id, github}) =>
+`<div class="container">
+<h1 class="name">Name: ${name}</h1>
+<h1 class="position>Position: Engineer</h1>
+<h2 class="email">Email: ${email}</h2>
+<h2 class="id">ID: ${id}</h2>
+<h2 class="github">Github: ${github}</h2>
+`;
+
+const internHTML = ({name, email, id, school}) =>
+`<div class="container">
+<h1 class="name">Name: ${name}</h1>
+<h1 class="position>Position: Intern</h1>
+<h2 class="email">Email: ${email}</h2>
+<h2 class="id">ID: ${id}</h2>
+<h2 class="github">School: ${school}</h2>
+`;
+
+const init = () => {
+    initialPrompt()
+    .then(answer => {
+        if(answer === 'Manager') {
+            managerPrompt();
+        } else if (answer === 'Engineer') {
+            engineerPrompt();
+        } else if (answer === 'Intern') {
+            internPrompt();
+        }
+    })
+    .catch((err) => console.error(err));
+}
+
+init();
